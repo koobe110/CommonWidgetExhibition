dce6e0922c607e108facf0766d8f88dcd67340c3
longxi.xu <longxi.xu@cootek.cn>
2016-07-10 14:23:33 +0800

    time widget is good

---
+++ app/src/main/java/com/along/myapplication/widget/WeatherWidget.java
@@ -0,0 +1,48 @@
+package com.along.myapplication.widget;
+
+import android.appwidget.AppWidgetManager;
+import android.appwidget.AppWidgetProvider;
+import android.content.Context;
+import android.content.Intent;
+import android.widget.RemoteViews;
+
+import com.along.myapplication.R;
+
+/**
+ * Implementation of App Widget functionality.
+ */
+public class WeatherWidget extends AppWidgetProvider {
+
+    static void updateAppWidget(Context context, AppWidgetManager appWidgetManager,
+                                int appWidgetId) {
+
+        CharSequence widgetText = "天气";
+        // Construct the RemoteViews object
+        RemoteViews views = new RemoteViews(context.getPackageName(), R.layout.weather_widget);
+        views.setTextViewText(R.id.weather_text, widgetText);
+
+        // Instruct the widget manager to update the widget
+        appWidgetManager.updateAppWidget(appWidgetId, views);
+    }
+
+    @Override
+    public void onUpdate(Context context, AppWidgetManager appWidgetManager, int[] appWidgetIds) {
+        // There may be multiple widgets active, so update all of them
+        for (int appWidgetId : appWidgetIds) {
+            updateAppWidget(context, appWidgetManager, appWidgetId);
+        }
+    }
+
+    @Override
+    public void onEnabled(Context context) {
+        // Enter relevant functionality for when the first widget is created
+        context.startService(new Intent(context,WeatherService.class));
+    }
+
+    @Override
+    public void onDisabled(Context context) {
+        // Enter relevant functionality for when the last widget is disabled
+        context.stopService(new Intent(context,WeatherService.class));
+    }
+}
+
